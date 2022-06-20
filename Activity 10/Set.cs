e21d49ec7ebcdd7b9155952d3dd0a415bc42346d
Cory-Watt <106133667+Cory-Watt@users.noreply.github.com>
2022-06-20 02:10:35 -0400

    Add files via upload

---
+++ Activity 10/Set.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_10
+{
+        class Set
+        {
+            private List<int> elements;
+
+            public Set()
+            {
+                elements = new List<int>();
+            }
+
+            public bool addElement(int val)
+            {
+                if (containsElement(val)) return false;
+                else
+                {
+                    elements.Add(val);
+                    return true;
+                }
+            }
+
+            private bool containsElement(int val)
+            {
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    if (val == elements[i]) return true;
+                    //unnecessary else-return considering if condition evaluates false the default return will return false
+                    //else 
+                    //return false; 
+                }
+                return false;
+            }
+
+            public override string ToString()
+            {
+                string str = "";
+                foreach (int i in elements)
+                {
+                    str += i + " ";
+                }
+                return str;
+            }
+
+
+            public void clearSet()
+            {
+                elements.Clear();
+            }
+
+            public Set union(Set rhs)
+            {
+                //initialize new set to preserve the original the values can be displayed at runtime
+                Set newSet = new Set();
+
+                for (int i = 0; i < rhs.elements.Count; i++)
+                {
+                    newSet.addElement(rhs.elements[i]);
+                }
+                for (int i = 0; i < this.elements.Count; i++)
+                {
+                    newSet.addElement(this.elements[i]);
+                }
+                return newSet;
+            }
+        }
+}
